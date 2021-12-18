1814791576fce153a0d0c485c4671af967e53f6d
Kamil Tyugaev <samilkuaev@gmail.com>
2021-12-18 17:10:59 +0300

    Add TaskCell

---
+++ Model/Task.swift
@@ -0,0 +1,36 @@
+//
+//  Task.swift
+//  ToDoManager
+//
+//  Created by IosDeveloper on 18.12.2021.
+//
+
+import UIKit
+// требования к типу, описывающему сущность "Задача"
+protocol TaskProtocol {
+    var title: String { get set }
+    var type: TaskPriority { get set }
+    var status: TaskStatus { get set }
+}
+// сущность "Задача"
+struct Task: TaskProtocol {
+    var title: String
+    var type: TaskPriority
+    var status: TaskStatus
+}
+
+//тип задачи
+enum TaskPriority{
+    //текущая
+    case normal
+    //важнaя
+    case important
+}
+
+//состояние задачи
+enum TaskStatus{
+    //запланированная
+    case planned
+    //выполненная
+    case completed
+}
---
+++ Model/TaskStorage.swift
@@ -0,0 +1,33 @@
+//
+//  TaskStorage.swift
+//  ToDoManager
+//
+//  Created by IosDeveloper on 18.12.2021.
+//
+
+import UIKit
+// Протокол, описывающий сущность "Хранилище задач"
+protocol TaskStorageProtocol {
+    func loadTasks() -> [TaskProtocol]
+    func saveTasks(_ tasks:[TaskProtocol])
+}
+
+// Сущность "Хранилище задач"
+class TaskStorage: TaskStorageProtocol {
+   
+    
+    func loadTasks() -> [TaskProtocol] {
+            let testTask:[TaskProtocol] = [
+                Task(title: "write dz popsix", type: .important, status: .planned),
+                Task(title: "Change my", type: .normal, status: .planned),
+                Task(title: "work IOS", type: .normal, status: .planned),
+                Task(title: "Help family", type: .important, status: .planned),
+                Task(title: "Lern English", type: .important, status: .planned),
+                Task(title: "write k\r", type: .important, status: .completed),
+                Task(title: "eat apple", type: .normal, status: .planned)
+            ]
+            return testTask
+        }
+    
+    func saveTasks(_ tasks: [TaskProtocol]) {}
+}
